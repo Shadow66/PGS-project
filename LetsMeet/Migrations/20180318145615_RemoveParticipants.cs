b81368756577e1720f4f594dd531f864a778958f
Mateusz Janiszewski <mateusz2716@gmail.com>
2018-03-18 22:09:08 +0100

    database v1.0

---
+++ LetsMeet/Migrations/20180318145615_RemoveParticipants.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LetsMeet.Migrations
+{
+    public partial class RemoveParticipants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                });
+        }
+    }
+}
---
+++ LetsMeet/Models/Event.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsMeet.Models
+{
+    public enum Category
+    {
+        ArtsCulture,
+        BookClubs,
+        CareerBusiness,
+        CarsMotorcycles,
+        CommunityEnvironment,
+        Dancing,
+        EducationLearning,
+        FashionBeauty,
+        Fitness,
+        FoodDrink,
+        Games,
+        HealthWellbeing,
+        HobbiesCrafts,
+        LanguageEthnicIdentity,
+        Lifestyle,
+        MovementsPolitics,
+        MoviesFilm,
+        Music,
+        OutdoorsAdventure,
+        ParentsFamily,
+        PetsAnimals,
+        Photography,
+        ReligionBeliefs,
+        SciFiFantasy,
+        Singles,
+        Socializing,
+        SportsRecreation,
+        Support,
+        Tech,
+        Writing
+    }
+
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        //[ForeignKey()]
+        //public int UserId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+    }
+}
