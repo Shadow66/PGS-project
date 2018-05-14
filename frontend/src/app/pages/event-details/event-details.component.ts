81adec6c6cd42d8de68f3d7e479ae50e5aa04f9d
Adrian Wąż <226042@student.pwr.edu.pl>
2018-05-14 17:16:37 +0200

    eventDetails with "mock" function

---
+++ frontend/src/app/pages/event-details/event-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { EventModel } from '../../shared/models/event.model';
+import { SearchService } from '../../shared/services/search.service';
+import { EventListModel } from '../../shared/models/eventList.model';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-event-details',
+  templateUrl: './event-details.component.html',
+  styleUrls: ['./event-details.component.css']
+})
+export class EventDetailsComponent implements OnInit {
+  id: string;
+  event: EventModel;
+
+  constructor(
+    private route: ActivatedRoute,
+    private searchService: SearchService
+  ) {
+    this.route.params.subscribe((params: Params) => {
+      if (params['id'] === undefined) {
+        this.id = '';
+      } else {
+        this.id = '' + params['id'];
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    // this.searchService.getEvent(this.id);
+    // .subscribe(event => (this.event = event), error => console.log(error));
+    this.event = this.searchService.getEvent(this.id);
+  }
+
+}
