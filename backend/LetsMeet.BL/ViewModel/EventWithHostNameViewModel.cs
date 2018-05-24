87a2d08fde650fdab4b64e06166f821bf2e4d105
Adrian Wąż <226042@student.pwr.edu.pl>
2018-05-24 19:40:01 +0200

    event details using api

--- backend/LetsMeet.BL/ViewModel/EventWithHostNameViewModel.cs
+++ backend/LetsMeet.BL/ViewModel/EventWithHostNameViewModel.cs
@@ -10,7 +10,9 @@ namespace LetsMeet.BL.ViewModel
         public string HostName { get; set; }
         public string Address { get; set; }
         public string StartDate { get; set; }
+        public string EndDate { get; set; }
         public string Title { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
     }
 }
